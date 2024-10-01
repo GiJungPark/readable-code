f5472ceb3c6063c39990e4bf4d2d350c5a6ad879
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-01 16:44:45 +0900

    refactor: IllegalArgumentException에서 AppException을 사용하도록 리팩토링

--- src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
+++ src/main/java/cleancode/minesweeper/tobe/MinesweeperGame.java
@@ -43,8 +43,10 @@ public class MinesweeperGame {
                 String cellInput = getCellInputFromUser();
                 String userActionInput = getUserActionInputFromUser();
                 actOneCell(cellInput, userActionInput);
-            } catch (IllegalArgumentException e) {
+            } catch (AppException e) {
                 System.out.println(e.getMessage());
+            } catch (Exception e) {
+                System.out.println("프로그램에 문제가 생겼습니다.");
             }
         }
     }
@@ -71,7 +73,7 @@ public class MinesweeperGame {
             return;
         }
 
-        System.out.println("잘못된 번호를 선택하셨습니다.");
+        throw new AppException("잘못된 번호를 선택하셨습니다.");
     }
 
     private static void changeGameStatusToLose() {
@@ -140,7 +142,7 @@ public class MinesweeperGame {
     private static int convertRowFrom(char cellInputRow) {
         int rowIndex = Character.getNumericValue(cellInputRow) - 1;
         if (rowIndex >= BOARD_ROW_SIZE) {
-            throw new IllegalArgumentException("잘못된 입력입니다.");
+            throw new AppException("잘못된 입력입니다.");
         }
 
         return rowIndex;
@@ -169,7 +171,7 @@ public class MinesweeperGame {
             case 'j':
                 return 9;
             default:
-                throw new IllegalArgumentException("잘못된 입력입니다.");
+                throw new AppException("잘못된 입력입니다.");
         }
     }
 
