12c77705caac00afa5d46b139bb7f605c1d937a1
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-10-06 00:24:45 +0900

    refactor: Cell을 상속이 아닌 인터페이스로 변경해서 조합을 사용

--- src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
+++ src/main/java/cleancode/minesweeper/tobe/cell/Cell.java
@@ -1,32 +1,21 @@
 package cleancode.minesweeper.tobe.cell;
 
-public abstract class Cell {
-    protected static final String FLAG_SIGN = "⚑";
-    protected static final String UNCHECKED_SIGN = "□";
+public interface Cell {
+    String FLAG_SIGN = "⚑";
+    String UNCHECKED_SIGN = "□";
 
-    protected boolean isFlagged;
-    protected boolean isOpened;
+    boolean isLandMine();
 
-    public abstract boolean isLandMine();
+    boolean hasLandMineCount();
 
-    public abstract boolean hasLandMineCount();
-
-    public abstract String getSign();
+    String getSign();
 
 
-    public void flag() {
-        this.isFlagged = true;
-    }
+    void flag();
 
-    public void open() {
-        this.isOpened = true;
-    }
+    void open();
 
-    public boolean isChecked() {
-        return this.isFlagged || this.isOpened;
-    }
+    boolean isChecked();
 
-    public boolean isOpened() {
-        return this.isOpened;
-    }
+    boolean isOpened();
 }
